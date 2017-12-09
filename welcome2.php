06cc9a28e220f7c7354110e57429e779a509d699
pulkit83 <pulkitgarg1059@gmail.com>
2017-12-09 17:08:51 +0530

    hosted

--- welcome2.php
+++ welcome2.php
@@ -5,7 +5,9 @@
 		$usrname=$_POST["username"];
 		$psw=$_POST["password"];
 		$email=$_POST["email"];
-		$sql= "INSERT INTO accounts (username, password, email) VALUES ('$usrname', '$psw', '$email' )";
+		$hash = md5( rand(0,1000) );
+		$sql= "INSERT INTO accounts (username, password, email, hash) VALUES ('$usrname', '$psw', '$email', '$hash' )";
+
 		$qstat =mysqli_query($connection, $sql);
 		if(!$qstat)
 		{
@@ -16,7 +18,23 @@
 		{
 			session_start();	
 			$_SESSION['username'] = $usrname;	
-			header("Location: login1.php");
+
+/*
+			$to      = $email; // Send email to our user
+			$subject = 'Signup | Verification'; // Give the email a subject 
+			$message = '
+			Username: '.$usrname.'
+			Password: '.$psw.'
+			http://www.mnitquora.com/verify.php?email='.$email.'&hash='.$hash.' 
+			'; // Our message above including the link
+                     
+			$headers = 'From:pulkitgarg419@gmail.com' . "\r\n"; // Set from headers
+			$retval = mail ($to,$subject,$message,$headers);
+         
+        	
+*/
+
+			header("Location: main.php");
 			exit();
 		}
 	}
